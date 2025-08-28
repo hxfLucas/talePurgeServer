0bc9c955c9bc6a137ff6a799fa5b47b688b8ff69
Lucas Pereira <lucas.pereira@haxify.com>
2025-08-28 01:21:44 +0100

    shootable fireball

--- src/skills/mage/fireball/FireballSkill.ts
+++ src/skills/mage/fireball/FireballSkill.ts
@@ -18,7 +18,7 @@ export class FireballSkill {
         this.gameSkill.maxDistance = 10;
         this.gameSkill.projectileSpeed = 0.2;
         this.gameSkill.AOERadius = 5;
-        this.gameSkill.skillType = "THROWABLE";
+        this.gameSkill.skillType = "SHOOTABLE";
         this.gameSkill.requiresClassIdentifier = GameDataPlayerClassIdentifiersHelper().getMageClassIdentifier();
     }
 
